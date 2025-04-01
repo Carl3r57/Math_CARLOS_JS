c39d5b759dfb64d04e333604a90399a4f89a97cd
Carlos Pizzino <pizzino.carlos11@gmail.com>
2025-04-01 13:21:19 +0200

    Ejercicio 4

--- scripts.js
+++ scripts.js
@@ -12,5 +12,7 @@ let numeroAleatorio2 = Math.round(Math.random() * (numero2 - numero1) + numero1)
 console.log("Número aleatorio entre 5 y 10:", numeroAleatorio2);
 
 //4
-
+console.log("Math.sign(-10):", Math.sign(10)); 
+console.log("Math.sign(0):", Math.sign(0));  
+console.log("Math.sign(10):", Math.sign(-10)); 
 
